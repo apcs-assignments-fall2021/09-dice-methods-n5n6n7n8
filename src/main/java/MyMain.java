652844abc1436ec5c4a32701290768471acb3759
natewong <nwwong17@gmail.com>
2021-09-29 21:44:08 -0700

    Created methods for calculating dice roll probabilities for APCS HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,8 +2,7 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int) (Math.random()*6) + 1;
     }
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -11,32 +10,80 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-
-        // Your code should roll 6 dice 10,000 times, so you should have a
-        // for loop such as:
+        int successtrials=0;
         for (int i = 0; i < 10000; i++) {
-
+            Boolean pass=false;
+            for (int j = 0; j < 6; j++) {
+                if(rollDie()==6)
+                {
+                    pass=true;
+                    break;
+                }
+            }
+            if(pass) {
+                successtrials++;
+            }
         }
-
-        return -1.0;
+        return ((double) successtrials)/100;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int successtrials=0;
+        for (int i = 0; i < 10000; i++) {
+            Boolean pass=false;
+            int diecount=0;
+            for (int j = 0; j < 12; j++) {
+                if(rollDie()==6)
+                {
+                    diecount++;
+                }
+                if(diecount>=2)
+                {
+                    pass=true;
+                    break;
+                }
+            }
+            if(pass) {
+                successtrials++;
+            }
+        }
+        return ((double) successtrials)/100;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int successtrials=0;
+        for (int i = 0; i < 10000; i++) {
+            Boolean pass=false;
+            int diecount=0;
+            for (int j = 0; j < 18; j++) {
+                if(rollDie()==6)
+                {
+                    diecount++;
+                }
+                if(diecount>=3)
+                {
+                    pass=true;
+                    break;
+                }
+            }
+            if(pass) {
+                successtrials++;
+            }
+        }
+        return ((double) successtrials)/100;
     }
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println("Rolling 10 dice: ");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(rollDie());
+            }
+        System.out.println("Estimated probability for rolling one six out of six dice: " + probabilityOneSix());
+        System.out.println("Estimated probability for rolling two sixes out of twelve dice: " + probabilityTwoSixes());
+        System.out.println("Estimated probability for rolling three sixes out of eighteen dice: " + probabilityThreeSixes());
     }
 }
